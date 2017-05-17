21461ee0ca2c1531bec8578ad270ef59db283406
Alberto-PC\Alberto <miravete92@hotmail.com>
2017-05-17 22:42:44 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,65 @@
+var OMDB_BASE_URL = 'https://www.googleapis.com/youtube/v3/search/';
+var KEY = 'AIzaSyC6VRZhBxshBbLlBeuohK7NvJeqL0u5dt8';
+var nextPageToken;
+var prevPageToken;
+function getDataFromApi(searchTerm, pageToken, callback) {
+  var query = {
+    part: 'snippet',
+    key: KEY,
+    q: searchTerm,
+    pageToken: pageToken
+  }
+  $.getJSON(OMDB_BASE_URL, query, callback);
+}
+
+
+function displayOMDBSearchData(data) {
+  var resultElement = '';
+  console.log(data);
+  if (data.items.length>0) {
+    data.items.forEach(function(item) {
+        resultElement += 
+        '<div class="searchResult">'+
+          '<h3>' + item.snippet.title + '</h3>'+
+          '<a href="https://www.youtube.com/watch?v='+ item.id.videoId +'">' +
+            '<img src="' + item.snippet.thumbnails.medium.url + '" alt="thumbnail"></img>'+
+          '</a>' +
+          '<p>' + item.snippet.description + '</p>'+
+          '<a href="https://www.youtube.com/channel/'+item.snippet.channelId+'">More from this channel</a>'+
+        '</div>';
+    });
+    if(data.prevPageToken){
+      resultElement += '<button class="prevPage">NextPage</button>';
+      prevPageToken = data.prevPageToken;
+    }
+    if(data.nextPageToken){
+      resultElement += '<button class="nextPage">NextPage</button>';
+      nextPageToken = data.nextPageToken;
+    }
+  }
+  else {
+    resultElement += '<p>No results</p>';
+  }
+  
+  $('.js-search-results').html(resultElement);
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(function(e) {
+    e.preventDefault();
+    var query = $(this).find('.js-query').val();
+    getDataFromApi(query, null, displayOMDBSearchData);
+  });
+
+  $('.js-search-form').on("click",".prevPage",function(e) {
+    e.preventDefault();
+    var query = $(this).find('.js-query').val();
+    getDataFromApi(query, prevPageToken, displayOMDBSearchData);
+  });
+  $('.js-search-results').on("click",".nextPage",function(e) {
+    e.preventDefault();
+    var query = $(this).find('.js-query').val();
+    getDataFromApi(query, nextPageToken, displayOMDBSearchData);
+  });
+}
+$(function(){watchSubmit();});
